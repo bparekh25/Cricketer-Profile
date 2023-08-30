686fa28ac4a1d3e4420d13030ae84d89bc172a3c
bparekh25 <131433664+bparekh25@users.noreply.github.com>
2023-08-30 22:05:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+var add = document.querySelector("#add")
+var remove = document.querySelector("#remove")
+var friend = document.querySelector("h5")
+
+add.addEventListener("click",function(){
+    friend.innerHTML = "Friend"
+    friend.style.color = "green"
+})
+remove.addEventListener("click",function(){
+    friend.innerHTML = "Unfriend"
+    friend.style.color = "red"
+})
+
+var add1 = document.querySelector("#add1")
+var remove1 = document.querySelector("#remove1")
+var friend1 = document.querySelector("#profile1")
+
+add1.addEventListener("click",function(){
+    friend1.innerHTML = "Friend"
+    friend1.style.color = "green"
+})
+remove1.addEventListener("click",function(){
+    friend1.innerHTML = "Unfriend"
+    friend1.style.color = "red"
+})
+
+var add2 = document.querySelector("#add2")
+var remove2 = document.querySelector("#remove2")
+var friend2 = document.querySelector("#profile2")
+
+add2.addEventListener("click",function(){
+    friend2.innerHTML = "Friend"
+    friend2.style.color = "green"
+})
+remove2.addEventListener("click",function(){
+    friend2.innerHTML = "Unfriend"
+    friend2.style.color = "red"
+})
+
+var add3 = document.querySelector("#add3")
+var remove3 = document.querySelector("#remove3")
+var friend3 = document.querySelector("#profile3")
+
+add3.addEventListener("click",function(){
+    friend3.innerHTML = "Friend"
+    friend3.style.color = "green"
+})
+remove3.addEventListener("click",function(){
+    friend3.innerHTML = "Unfriend"
+    friend3.style.color = "red"
+})
+var add4 = document.querySelector("#add4")
+var remove4 = document.querySelector("#remove4")
+var friend4 = document.querySelector("#profile4")
+
+add4.addEventListener("click",function(){
+    friend4.innerHTML = "Friend"
+    friend4.style.color = "green"
+})
+remove4.addEventListener("click",function(){
+    friend4.innerHTML = "Unfriend"
+    friend4.style.color = "red"
+})
